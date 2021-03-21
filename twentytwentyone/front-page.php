b03318fb97bc54bb830b8d8ee0a9b8fd447dfa3b
vovkaSviter <vovka.sviter@gamil.com>
2021-03-21 11:46:03 +0500

    Test Loop for cat.3 Front page.

--- twentytwentyone/front-page.php
+++ twentytwentyone/front-page.php
@@ -1,1 +1,33 @@
-kkk
+<?php
+
+$args = array(
+  'cat'         => '2',
+);
+
+$q = new WP_Query($args);
+
+/*if ( $q->have_posts() ) { 
+  while ( $q->have_posts() ) {
+    $q->the_post();
+  } else {
+	//no post found
+  }
+  
+  wp_reset_postdata();
+  
+}*/
+
+if ( $q->have_posts() ) : 
+    while ( $q->have_posts() ) : $q->the_post();
+        the_title();
+		the_content();
+    endwhile;
+else :
+    _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+	
+	wp_reset_postdata();
+	
+endif;
+
+
+?>
